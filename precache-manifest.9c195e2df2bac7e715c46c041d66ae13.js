3be5f136d59a42aa4784ab3555cc1e67ce3c2fff
DemonCyborg <64414438+DemonCyborg@users.noreply.github.com>
2020-11-01 17:36:48 +0530

    Add files via upload

---
+++ precache-manifest.9c195e2df2bac7e715c46c041d66ae13.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "85dbad666b62cb79ef00a457942e8704",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7729073d68f1e2cddd68",
+    "url": "/static/css/2.eb43dee1.chunk.css"
+  },
+  {
+    "revision": "c75aa4dbea070cb4d2dc",
+    "url": "/static/css/main.fbf31756.chunk.css"
+  },
+  {
+    "revision": "7729073d68f1e2cddd68",
+    "url": "/static/js/2.4fc647d8.chunk.js"
+  },
+  {
+    "revision": "ca962714ee3537eb863ad2954c33c63e",
+    "url": "/static/js/2.4fc647d8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c75aa4dbea070cb4d2dc",
+    "url": "/static/js/main.edbe9ac7.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "08b3db12f70f41bd5074d5a6cbbb9285",
+    "url": "/static/media/amazon-clone.08b3db12.JPG"
+  },
+  {
+    "revision": "294e25d6c2ffeb4a1166c807da6b45cf",
+    "url": "/static/media/application.294e25d6.png"
+  },
+  {
+    "revision": "3686a09b297346896b39841f32e6c1cb",
+    "url": "/static/media/covid-tracker.3686a09b.JPG"
+  },
+  {
+    "revision": "30b8e70ec1114b36e12d888fefef1447",
+    "url": "/static/media/drum-kit.30b8e70e.JPG"
+  },
+  {
+    "revision": "96198f99b5bea8e0057fb2544b242252",
+    "url": "/static/media/keeper-app.96198f99.JPG"
+  },
+  {
+    "revision": "734b9510e2c7705d901b56256c7bf89d",
+    "url": "/static/media/personal-site.734b9510.JPG"
+  },
+  {
+    "revision": "962290d46affc4f97163855f7ccc42ee",
+    "url": "/static/media/profile.962290d4.png"
+  },
+  {
+    "revision": "0422b0f6d3f44fdefc82be01946e8538",
+    "url": "/static/media/responsive.0422b0f6.png"
+  },
+  {
+    "revision": "92e31c6853b3cca0301844e38118d932",
+    "url": "/static/media/simon-game.92e31c68.JPG"
+  },
+  {
+    "revision": "065d49c5f1e15b84a9e4e63663e8f6ae",
+    "url": "/static/media/tindog.065d49c5.JPG"
+  },
+  {
+    "revision": "ebc9bb074075eda55626794f17ce9cf6",
+    "url": "/static/media/www (1).ebc9bb07.png"
+  }
+]);
